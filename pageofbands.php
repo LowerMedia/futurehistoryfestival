fc76e7ff7b291adfdcb5d9ab91df07f0eb4adce4
Pete Lower <pete.lower@gmail.com>
2013-02-27 16:07:28 -0600

    Editing last created page

--- pageofbands.php
+++ pageofbands.php
@@ -7,30 +7,23 @@
 ?>
 
 <?php get_header(); ?>
-
-        <div id="container">
-            <div id="content">
-
-<?php
+    <div id="container">
+      <div id="content">
+<?php 
 $type = 'fhf_band';
 $args=array(
-  'post_type' => $type,
-  'post_status' => 'publish',
-  'posts_per_page' => -1,
-  'caller_get_posts'=> 1
-
-$my_query = null;
-$my_query = new WP_Query($args);
-if( $my_query->have_posts() ) {
-  while ($my_query->have_posts()) : $my_query->the_post(); ?>
-    <p><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></p>
-    <?php
-  endwhile;
-}
-wp_reset_query();  // Restore global post data stomped by the_post().
+ 'post_type' => $type,
+ 'post_status' => 'publish',
+ 'paged' => $paged,
+ 'posts_per_page' => 2,
+ 'ignore_sticky_posts'=> 1
+);
+$temp = $wp_query; // assign ordinal query to temp variable for later use  
+$wp_query = null;
+$wp_query = new WP_Query($args); 
 ?>
-            </div><!-- #content -->
-        </div><!-- #container -->
-
+<?php get_template_part( 'loop', 'index' );?>
+  </div><!-- #content -->
+</div><!-- #container -->
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
