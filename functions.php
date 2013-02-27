445abf8d9d934e1db370d0e571abe298eeb9db24
Pete Lower <pete.lower@gmail.com>
2013-02-27 13:48:39 -0600

    Functions files cleanup and adding js

--- functions.php
+++ functions.php
@@ -1,48 +1,54 @@
 <?php
+
+/*
+#
+#   CREATE BAND POST TYPE
+#   //This function creates a custom post type of band
+*/
+
 add_action( 'init', 'create_post_type' );
-function create_post_type() {
-	register_post_type( 'fhf_band',
-		array(
-			'labels' => array(
-				'name' => __( 'Band' ),
-				'singular_name' => __( 'Band' )
-			),
-		'public' => true,
-		'has_archive' => true,
-		)
-	);
-}
+function create_post_type() 
+  {
+  	register_post_type( 'fhf_band',
+  		array(
+  			'labels' => array(
+  				'name' => __( 'Band' ),
+  				'singular_name' => __( 'Band' )
+  			),
+  		'public' => true,
+  		'has_archive' => true,
+  		)
+  	);
+  }
 
-//function theme_styles()  
-//{ 
-  // Register the style like this for a theme:  
-  // (First the unique name for the style (custom-style) then the src, 
-  // then dependencies and ver no. and media type)
- // wp_register_style( 'screen', get_stylesheet_directory_uri() . '/stylesheets/screen.css', array(), '20120208', 'all' );
+/*
+#
+#   ADD CUSTOM JS FILES/LIBRARIES(STICKY.JS)
+#   //This function adds custom javascript libraries and files
+*/
 
-  //wp_register_style( 'sass-print', get_stylesheet_directory_uri() . '/stylesheets/print.css', array(), '20120208', 'all' );
+function lowermedia_add_theme_js()  
+  {  
+      // Register and enque sticky.js the script like this for a theme:  
+      // Sticky JS http://www.labs.anthonygarand.com/sticky
+      wp_register_script( 'sticky', get_stylesheet_directory_uri() . '/js/sticky.js', array( 'jquery' ) );  
+      wp_enqueue_script( 'sticky' );  
+  }  
+add_action( 'wp_enqueue_scripts', 'wptuts_scripts_with_jquery' ); 
 
-  //wp_register_style( 'sass-ie', get_stylesheet_directory_uri() . '/stylesheets/ie.css', array(), '20120208', 'all' );
+/*
+#
+#   ADD CUSTOM CSS FILES (SASS)
+#   //This function adds custom css and our compiled sass files (css)
+*/
 
-  // enqueing:
-  //wp_enqueue_style( 'sass-ie' );
-  //wp_enqueue_style( 'sass-print' );
-  //wp_enqueue_style( 'screen' );
-//}
-//add_action('wp_enqueue_scripts', 'theme_styles');
-
-//This function adds our compiles sass files (css) to the header
-function lowermedia_add_theme_sass()  
-{  
-    // Register the style like this for a plugin:  
-    //wp_register_style( 'screen', plugins_url( '/stylesheets/custom-style.css', __FILE__ ), array(), '20120208', 'all' );  
-    // or  
-    // Register the style like this for a theme:  
-    //wp_register_style( 'screen', get_template_directory_uri() . '/stylesheets/screen.css', array(), '20120208', 'all' );  
-    wp_register_style( 'screen', get_stylesheet_directory_uri() . '/stylesheets/screen.css', array(), '20120208', 'all' );  
-  
-    // For either a plugin or a theme, you can then enqueue the style:  
-    wp_enqueue_style( 'screen' );  
-}  
+function lowermedia_add_theme_css()  
+  {  
+      // Register and enque the compiled sass stylesheet screen.css like this for a theme:  
+      wp_register_style( 'screen', get_stylesheet_directory_uri() . '/stylesheets/screen.css', array(), '20120208', 'all' );   
+      wp_enqueue_style( 'screen' );  
+  }  
 add_action( 'wp_enqueue_scripts', 'lowermedia_add_theme_sass' );  
+
+
 ?>
