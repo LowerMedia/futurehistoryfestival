03a60ae85134c0a021f7467c54d59c2974c9bd6b
Pete Lower <pete.lower@gmail.com>
2013-02-27 11:37:46 -0600

    More functions edits to add SASS

--- includes/functions.php
+++ includes/functions.php
@@ -697,12 +697,12 @@ endif;
 
 
 
-function theme_styles()  
-{ 
+//function theme_styles()  
+//{ 
   // Register the style like this for a theme:  
   // (First the unique name for the style (custom-style) then the src, 
   // then dependencies and ver no. and media type)
-  wp_register_style( 'screen', get_stylesheet_directory_uri() . '/stylesheets/screen.css', array(), '20120208', 'all' );
+ // wp_register_style( 'screen', get_stylesheet_directory_uri() . '/stylesheets/screen.css', array(), '20120208', 'all' );
 
   //wp_register_style( 'sass-print', get_stylesheet_directory_uri() . '/stylesheets/print.css', array(), '20120208', 'all' );
 
@@ -711,8 +711,23 @@ function theme_styles()
   // enqueing:
   //wp_enqueue_style( 'sass-ie' );
   //wp_enqueue_style( 'sass-print' );
-  wp_enqueue_style( 'screen' );
-}
-add_action('wp_enqueue_scripts', 'theme_styles');
+  //wp_enqueue_style( 'screen' );
+//}
+//add_action('wp_enqueue_scripts', 'theme_styles');
+
+//This function adds our compiles sass files (css) to the header
+function lowermedia_add_theme_sass()  
+{  
+    // Register the style like this for a plugin:  
+    //wp_register_style( 'screen', plugins_url( '/stylesheets/custom-style.css', __FILE__ ), array(), '20120208', 'all' );  
+    // or  
+    // Register the style like this for a theme:  
+    //wp_register_style( 'screen', get_template_directory_uri() . '/stylesheets/screen.css', array(), '20120208', 'all' );  
+    wp_register_style( 'screen', get_stylesheet_directory_uri() . '/stylesheets/screen.css', array(), '20120208', 'all' );  
+  
+    // For either a plugin or a theme, you can then enqueue the style:  
+    wp_enqueue_style( 'screen' );  
+}  
+add_action( 'wp_enqueue_scripts', 'lowermedia_add_theme_sass' );  
 
 ?>
