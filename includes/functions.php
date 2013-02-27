afb06b498ed560eda12f4ed2be9f4ae98b686fb9
Pete Lower <pete.lower@gmail.com>
2013-02-26 18:04:39 -0600

    Adding sass to theme

--- includes/functions.php
+++ includes/functions.php
@@ -693,4 +693,37 @@ endif;
     }
 	
     add_action('widgets_init', 'responsive_widgets_init');
+
+
+
+function theme_styles()  
+{ 
+  // Register the style like this for a theme:  
+  // (First the unique name for the style (custom-style) then the src, 
+  // then dependencies and ver no. and media type)
+  wp_register_style( 'screen', 
+    get_template_directory_uri() . '/stylesheets/screen.css', 
+    array(), 
+    '20120208', 
+    'all' );
+
+  wp_register_style( 'print', 
+    get_template_directory_uri() . '/stylesheets/print.css', 
+    array(), 
+    '20120208', 
+    'all' );
+
+  wp_register_style( 'ie', 
+    get_template_directory_uri() . '/stylesheets/ie.css', 
+    array(), 
+    '20120208', 
+    'all' );
+
+  // enqueing:
+  wp_enqueue_style( 'ie.css' );
+  wp_enqueue_style( 'print.css' );
+  wp_enqueue_style( 'screen.css' );
+}
+add_action('wp_enqueue_scripts', 'theme_styles');
+
 ?>
