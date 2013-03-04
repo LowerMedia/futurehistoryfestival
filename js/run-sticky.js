16da84850a30db84160ebfcd5bd3b643c05543d9
Pete Lower <pete.lower@gmail.com>
2013-03-04 11:38:03 -0600

    Editing top margins on run-sticky.js

--- js/run-sticky.js
+++ js/run-sticky.js
@@ -1,3 +1,7 @@
 jQuery(document).ready(function(){
+<<<<<<< HEAD
   jQuery("#nav-wrapper").sticky({ topSpacing: 28 });
+=======
+  jQuery("#nav-wrapper").sticky({ topSpacing: 0 });
+>>>>>>> Editing top margins on run-sticky.js
 });
