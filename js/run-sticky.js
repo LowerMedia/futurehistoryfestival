aa8e5b6d36350ec5da0c7f047c541871a4299393
hawkeye126 <pete.lower@gmail.com>
2013-03-04 10:42:52 -0700

    fixing js

--- js/run-sticky.js
+++ js/run-sticky.js
@@ -1,7 +1,3 @@
 jQuery(document).ready(function(){
-<<<<<<< HEAD
-  jQuery("#nav-wrapper").sticky({ topSpacing: 28 });
-=======
   jQuery("#nav-wrapper").sticky({ topSpacing: 0 });
->>>>>>> Editing top margins on run-sticky.js
 });
